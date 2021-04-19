54af1908ef3b2ec66ed598f280d8086ed4dbfb08
Philipp Gawlik <philipp.gawlik@posteo.de>
2021-04-19 17:28:18 +0200

    Added example prompt

---
+++ prompts/example_prompt.py
@@ -0,0 +1,18 @@
+QUESTION_PROMPT = "This is a prompt with {}"
+ANSWER_PROMPT = "This is a prompt with {}"
+QUESTION_SETTINGS = {
+    "temperature": 1.0,
+    "max_tokens": 10,
+    "top_p": 1.0,
+    "frequency_penalty": 0.1,
+    "presence_penalty": 0.5,
+    "stop": ["\n\n"]
+}
+ANSWER_SETTINGS = {
+    "temperature": 1.0,
+    "max_tokens": 10,
+    "top_p": 1.0,
+    "frequency_penalty": 0.1,
+    "presence_penalty": 0.5,
+    "stop": ["\n\n"]
+}
